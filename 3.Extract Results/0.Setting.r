c4d1b2e0c042fe33cfdfe3ab7917c478665217dd
IDOJI <112312608+IDOJI@users.noreply.github.com>
2024-08-15 18:03:52 +0900

    Extract results

---
+++ 3.Extract Results/0.Setting.r
@@ -0,0 +1,58 @@
+# 🟥 Load Functions & Packages ##########################################################################
+
+Sys.setlocale("LC_ALL", "en_US.UTF-8")
+
+## 🟨Install and loading Packages ================================
+# rm(list = ls())
+install_packages = function(packages, load=TRUE) {
+  # load : load the packages after installation?
+  for(pkg in packages) {
+    if (!require(pkg, character.only = TRUE)) {
+      install.packages(pkg)
+    }
+    
+    if(load){
+      library(pkg, character.only = TRUE, quietly = T)
+    }
+  }
+}
+
+List.list = list()
+List.list[[1]] = visual = c("ggpubr", "ggplot2", "ggstatsplot", "ggsignif", "rlang", "RColorBrewer")
+List.list[[2]] = stat = c("fda", "MASS")
+List.list[[3]] = data_handling = c("tidyverse", "dplyr", "clipr", "tidyr", "readr", "caret", "readxl")
+List.list[[4]] = qmd = c("janitor", "knitr")
+List.list[[5]] = texts = c("stringr", "stringi")
+List.list[[6]] = misc = c("devtools")
+List.list[[7]] = db = c("RMySQL", "DBI", "odbc", "RSQL", "RSQLite")
+List.list[[8]] = sampling = c("rsample")
+List.list[[9]] = excel = c("openxlsx")
+List.list[[10]] = others = c("beepr")
+
+packages_to_install_and_load = unlist(List.list)
+install_packages(packages_to_install_and_load)
+
+## 🟧dplyr ==================================================================================================
+filter = dplyr::filter
+select = dplyr::select
+
+
+
+
+
+# 🟥 =========================================================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
